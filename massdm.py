a7409e68d37569a73a034d62d1e6c2af0ccc4cc2
9n8 <87565211+9n8@users.noreply.github.com>
2022-01-24 15:33:37 +0300

    Update massdm.py

--- massdm.py
+++ massdm.py
@@ -1,14 +1,17 @@
 import discord
 
+token = input("Enter Token: ")
+txt = input("Message: ")
+
 client = discord.Client()
 
 @client.event
 async def on_connect():
   for user in client.user.friends:
     try:
-      await user.send('Your Message')
+      await user.send(txt)
       print(f"messaged: {user.name}")
     except:
        print(f"couldn't message: {user.name}")
 
-client.run('Your Token', bot=False)
+client.run(token, bot=False)
